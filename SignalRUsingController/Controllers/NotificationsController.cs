94163218ef5670d0f06fdf7dea87435587a5dbe5
AVALON LABS <team@avalon-lbs.com>
2024-09-02 22:34:10 +0100

    Add project files.

---
+++ SignalRUsingController/Controllers/NotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRUsingController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationsController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendNotification([FromBody] NotificationModel notification)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", notification.User,notification.Message);
+            return Ok();
+        }
+
+        public class NotificationModel
+        {
+            public string Message { get; set; }
+            public string User { get; set; }
+        }
+    }
+}
---
+++ SignalRUsingController/NotificationHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRUsingController
+{
+    public class NotificationHub:Hub
+    {
+        private readonly ILogger<NotificationHub> _logger;
+
+        public NotificationHub(ILogger<NotificationHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            _logger.LogInformation($"Client connected: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
